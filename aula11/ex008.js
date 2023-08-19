3ed77b28f10836115208312bc4ac1c14e557125b
sandrivitorino <118567140+sandrivitorino@users.noreply.github.com>
2023-08-19 00:40:31 -0300

    add

--- aula11/ex008.js
+++ aula11/ex008.js
@@ -1,4 +1,4 @@
-var vel = 61
+var vel = 60
 console.log(`A velocidade do seu carro é ${vel} km/h`)
 if(vel > 60){ //condição simples!
     console.log('você ultrapassou a velocidade permitida. MULTADO!')
