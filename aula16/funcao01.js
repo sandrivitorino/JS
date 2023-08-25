8d5495bbf19c869df446388db736741d6b68e5d0
sandrivitorino <118567140+sandrivitorino@users.noreply.github.com>
2023-08-25 01:00:41 -0300

    funções

---
+++ aula16/funcao01.js
@@ -0,0 +1,10 @@
+    function parimpar(n){
+        if (n%2 == 0){
+            return 'Par'
+        } else {
+            return 'Impar'
+        }
+    }
+
+    console.log(parimpar(4))   /* OU =  let res = parimpar(2) */
+   
