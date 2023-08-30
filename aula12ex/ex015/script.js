ce8e644347f4908fa53c5562855c010127e5ee5a
sandrivitorino <118567140+sandrivitorino@users.noreply.github.com>
2023-08-30 15:03:14 -0300

    verificação de idade criado

--- aula12ex/ex015/script.js
+++ aula12ex/ex015/script.js
@@ -9,12 +9,43 @@ function verificar(){
         var fsex = document.getElementsByName('radsex')
         var idade = ano - Number(fano.value)
         var genero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', '')
         if (fsex[0].checked){
         genero = 'Homem'
+        if (idade >= 0 && idade < 14){
+            //criança
+            img.setAttribute('src', 'bebemenino.png')
+            
+        } else if (idade < 21){
+            //jovem
+            img.setAttribute('src', 'jovenmenino.png')
+        } else if (idade < 47){
+            //adulto
+            img.setAttribute('src', 'adultohomem.png')
+        } else {
+            //idoso
+            img.setAttribute('src', 'idodohomem.png')
+        }
     } else if (fsex[1].checked){
         genero = 'Mulher'
+        if (idade >= 0 && idade < 14){
+            //criança
+            img.setAttribute('src', 'bebemenina.png')
+        } else if (idade < 21){
+            //jovem
+            img.setAttribute('src', 'jovenmenina.png')
+        } else if (idade < 47){
+            //adulto
+            img.setAttribute('src', 'adultomulher.png')
+        } else {
+            //idoso
+            img.setAttribute('src', 'idosomulher.png')
+        }
     }
+    res.style.textAlign = 'center'
     res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+    res.appendChild(img)
 }
 
 }
